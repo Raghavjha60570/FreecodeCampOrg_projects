883bdff898afe32830a7335150b25d9e6f11b77a
Melisandre_64 <Rjha60571@gmail.com>
2024-12-01 20:08:26 +0530

    finally made

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -1,10 +1,36 @@
 const textInput= document.getElementById('text-input');
-const checkBtn=document.getElementById('check-btn');
+const checkBtn = document.getElementById('check-btn');
+const resultDisplay=document.getElementById('result');
+    
+      function isPal(value){
+         let str =value.toString(); 
+         let left=0 ;
+         let right=str.length-1;
+         while(left<right){
+            if(str[left]!==str[right]){
+                return false;
+            }
+            left++;
+            right--;
+         }
+         return true;
+      }
 
 checkBtn.addEventListener( 'click',function(){
   if(textInput.value.trim()===''){
     alert('Please Enter a value Before Submitting');
   }else {
-                alert('Form submitted successfully!');
+               const value= textInput.value.trim();
+               
+               const pal= isPal(value);
+               
+            if (pal){
+                resultDisplay.textContent = `"${value}" is a palindrome!`;
+                resultDisplay.style.color = 'green';
+            }else {
+                resultDisplay.textContent = `"${value}" is  not a palindrome!`;
+                resultDisplay.style.color = 'red';
             }
-})
+     }
+
+    })
